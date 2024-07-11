fabf2ded717be4a8b12022bfde7c476843bcf370
Damián <grimaldidamian001@gmail.com>
2024-07-11 20:24:31 -0300

    Creacion de los archivos necesarios para el editor de texto

---
+++ pantalla/pantalla.py
@@ -0,0 +1,49 @@
+import pygame
+import os
+
+class pantalla():
+    
+    def __init__(self,ancho,alto) -> None:
+        pass
+    
+    def manejo_texto(self):
+        """
+        Se encarga de mostrar en pantalla el texto ingresado por el usuario.add()
+        """
+        pass
+    
+    def manejo_teclas(self):
+        """
+        Manejo del teclado, para guardar el archivo es control + g
+        """
+        pass
+    
+    def comprobacion_archivo(self):
+        """
+        Aca va a validar si hay cambios con respecto al archivo
+        """
+        pass
+    
+    def guardado(self):
+        """
+        Se encarga de guardar el contenido en el archivo
+        """
+        pass
+    
+    def abrir_archivo(self):
+        """
+        Se encarga de abrir y tanscribir el contenido en el input para el usuario
+        """
+        pass
+    
+    def menu(self):
+        """
+        Elegir entre dos estado, abrir un archivo ya existente, o crear uno nuevo
+        """
+        pass
+    
+    def dibujar(self):
+        """
+        Se encarga de dibujar todo el contenido necesario en pantalla
+        """
+        pass
